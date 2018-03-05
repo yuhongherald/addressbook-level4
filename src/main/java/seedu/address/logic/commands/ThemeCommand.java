4535b7724f9ee81b8a1630be101e0171096f5282
DESKTOP-HNVCJP7\ZhenWei <e0032088@u.nus.edu>
2018-03-05 19:59:32 +0800

    Add new class ThemeCommand

---
+++ src/main/java/seedu/address/logic/commands/ThemeCommand.java
@@ -0,0 +1,49 @@
+package seedu.address.logic.commands;
+
+//@@author owzhenwei
+
+import seedu.address.commons.core.EventsCenter;
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.exceptions.CommandException;
+
+/**
+ * Changes the theme
+ */
+public class ThemeCommand extends Command{
+    public static final String COMMAND_WORD = "theme";
+
+    private static final int NUMBER_OF_THEMES = 3;
+
+    private static final String MESSAGE_THEME_CHANGE_SUCCESS = "Theme updated: %1$s";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Applies selected theme\n"
+            + "1. Default theme\n"
+            + "2. Teal theme\n"
+            + "3. Dark theme\n"
+            + "Parameters: INDEX (positive integer)\n"
+            + "Example: " + COMMAND_WORD + " 3";
+
+    private final Index selectedIndex;
+
+    public ThemeCommand(Index selectedIndex) {
+        this.selectedIndex = selectedIndex;
+    }
+
+    @Override
+    public CommandResult execute() throws CommandException {
+        if (selectedIndex.getZeroBased() >= NUMBER_OF_THEMES) {
+            throw new CommandException(Messages.MESSAGE_INVALID_THEME_INDEX);
+        }
+        return new CommandResult(String.format(MESSAGE_THEME_CHANGE_SUCCESS, selectedIndex.getOneBased()));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof ThemeCommand // instanceof handles nulls
+                && this.selectedIndex.equals(((ThemeCommand) other).selectedIndex)); // state check
+
+    }
+}
