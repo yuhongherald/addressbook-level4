70e15c56b9f054f04909f31e7d66294843119943
Yu Hong Herald <yuhongherald@gmail.com>
2018-03-06 22:11:38 +0800

    Some minor edits.

--- src/main/java/seedu/address/logic/commands/exceptions/CommandWordException.java
+++ src/main/java/seedu/address/logic/commands/exceptions/CommandWordException.java
@@ -3,7 +3,7 @@ package seedu.address.logic.commands.exceptions;
 import seedu.address.logic.commands.CommandWords;
 
 /**
- * Represents an error which occurs during execution of a {@link Command}.
+ * Represents an error which occurs during execution of {@link SetCommand}.
  */
 public class CommandWordException extends Exception {
     public CommandWordException(String message) {
