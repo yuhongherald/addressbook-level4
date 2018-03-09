cc2366084763505c96abe09d02e4ab407cde2a89
whenzei <e0032088@u.nus.edu>
2018-03-09 20:07:50 +0800

    Remove unused imports

--- src/main/java/seedu/address/model/job/VehicleNumber.java
+++ src/main/java/seedu/address/model/job/VehicleNumber.java
@@ -1,6 +1,5 @@
 package seedu.address.model.job;
 
-import static java.util.Objects.hash;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.AppUtil.checkArgument;
 
