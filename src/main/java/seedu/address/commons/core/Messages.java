ecec3d0dd459a3b4e91bbfd29ca374e8ccba3582
whenzei <e0032088@u.nus.edu>
2018-03-10 00:50:29 +0800

    Add new constant in Messages class

--- src/main/java/seedu/address/commons/core/Messages.java
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -10,5 +10,5 @@ public class Messages {
     public static final String MESSAGE_INVALID_PERSON_DISPLAYED_INDEX = "The person index provided is invalid";
     public static final String MESSAGE_PERSONS_LISTED_OVERVIEW = "%1$d persons listed!";
     public static final String MESSAGE_INVALID_THEME_INDEX = "The theme index provided is invalid";
-
+    public static final String MESSAGE_INVALID_FILE_PATH = "The file path is invalid";
 }
