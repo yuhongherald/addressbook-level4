b818180a11449eeca4a0a968f4d1fea64c855e58
whenzei <e0032088@u.nus.edu>
2018-03-17 17:49:31 +0800

    Fix checkstyle error

--- src/main/java/seedu/address/model/person/UniqueEmployeeList.java
+++ src/main/java/seedu/address/model/person/UniqueEmployeeList.java
@@ -3,10 +3,10 @@ package seedu.address.model.person;
 import static java.util.Objects.requireNonNull;
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
 
-import java.util.Comparator;
 import java.util.Collections;
-import java.util.Iterator;
+import java.util.Comparator;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
