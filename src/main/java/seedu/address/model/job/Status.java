d0af1bfc878d948764a0630b60f8d9a57b657b2f
whenzei <e0032088@u.nus.edu>
2018-03-17 16:15:23 +0800

    Add new Status class

---
+++ src/main/java/seedu/address/model/job/Status.java
@@ -0,0 +1,32 @@
+package seedu.address.model.job;
+
+/**
+ * Represents the status of a car servicing job
+ */
+public class Status {
+    public static final String STATUS_ONGOING = "ongoing";
+    public static final String STATUS_CLOSED = "closed";
+
+    public final String status;
+
+    public Status(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public String toString() {
+        return status;
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof Status // instanceof handles nulls
+                && this.status.equals(((Status) other).status)); // state check
+    }
+
+    @Override
+    public int hashCode() {
+        return status.hashCode();
+    }
+}
