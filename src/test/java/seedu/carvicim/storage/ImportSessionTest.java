d89205ceaff7485310000bfb8909732b2327771a
Yu Hong Herald <yuhongherald@gmail.com>
2018-04-02 16:37:17 +0800

    fix ImportSession test case

--- src/test/java/seedu/carvicim/storage/ImportSessionTest.java
+++ src/test/java/seedu/carvicim/storage/ImportSessionTest.java
@@ -1,29 +1,31 @@
 package seedu.carvicim.storage;
 
-import java.io.File;
-
 import org.junit.Test;
 
 import seedu.carvicim.storage.session.ImportSession;
 
 //@@author yuhongherald
 public class ImportSessionTest {
+    private static final String TEST_INPUT_FILE = "storage/session/ImportSessionTest/CS2103-testsheet.xlsx";
+    private static final String TEST_OUTPUT_FILE = "";
+
     @Test
     public void importTestFileWithErrorCorrection() throws Exception {
         ImportSession importSession = ImportSession.getInstance();
-        String path;
-        path = new File(".").getCanonicalPath();
-        importSession.initializeSession(
-                path + "\\src\\test\\resources\\model.session.ImportSessionTest\\CS2103-testsheet.xlsx");
+
+        ClassLoader classLoader = getClass().getClassLoader();
+        String path = classLoader.getResource(TEST_INPUT_FILE)
+                .getPath();
+        importSession.initializeSession(path);
         importSession.reviewAllRemainingJobEntries(true);
         importSession.closeSession();
     }
 
     private void compareExcelFiles() {
-
+        ;
     }
 
-    private void deleteFile(File file) {
+    private void deleteFile(String filePath) {
         ;
     }
 }
