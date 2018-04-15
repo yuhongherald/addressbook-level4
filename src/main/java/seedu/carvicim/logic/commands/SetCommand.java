eee4d528845f91b0d0f6a852a81e9c06c951b24c
Yu Hong Herald <yuhongherald@gmail.com>
2018-04-15 10:18:57 +0800

    Change set command message success

--- src/main/java/seedu/carvicim/logic/commands/SetCommand.java
+++ src/main/java/seedu/carvicim/logic/commands/SetCommand.java
@@ -17,7 +17,9 @@ public class SetCommand extends UndoableCommand {
             + "Parameters: CURRENT_COMMAND_WORD NEW_COMMAND_WORD\n"
             + "Example: " + COMMAND_WORD + " set st";
 
-    public static final String MESSAGE_SUCCESS = "%s has been replaced with %s!";
+    public static final String MESSAGE_ALIAS_SUCCESS = "%s has been replaced with %s!";
+    public static final String MESSAGE_DEFAULT_SUCCESS = "%s has been set as an alternative for %s!";
+    public static final String MESSAGE_REMOVE_ALIAS_SUCCESS = "%s has been removed!";
 
     private final String currentWord;
     private final String newWord;
@@ -30,8 +32,16 @@ public class SetCommand extends UndoableCommand {
         this.newWord = newWord;
     }
 
-    public String getMessageSuccess() {
-        return String.format(MESSAGE_SUCCESS, currentWord, newWord);
+    public String getMessageAliasSuccess() {
+        return String.format(MESSAGE_ALIAS_SUCCESS, currentWord, newWord);
+    }
+
+    public String getMessageRemoveAliasSuccess() {
+        return String.format(MESSAGE_REMOVE_ALIAS_SUCCESS, currentWord);
+    }
+
+    public String getMessageDefaultSuccess() {
+        return String.format(MESSAGE_DEFAULT_SUCCESS, newWord, currentWord);
     }
 
     public String getMessageUsed() {
@@ -50,7 +60,12 @@ public class SetCommand extends UndoableCommand {
         } catch (CommandWordException e) {
             throw new CommandException(e.getMessage());
         }
-        return new CommandResult(getMessageSuccess());
+        if (CommandWords.isDefaultCommandWord(currentWord)) {
+            return new CommandResult(getMessageDefaultSuccess());
+        } else if (CommandWords.isDefaultCommandWord(newWord)) {
+            return new CommandResult(getMessageRemoveAliasSuccess());
+        }
+        return new CommandResult(getMessageAliasSuccess());
     }
 
     @Override
